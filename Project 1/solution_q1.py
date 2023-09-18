1decc0c89efe15aeba8cc8698c96d33e5b097755
ladlad5 <gilad.stessel@gmail.com>
2023-09-18 09:31:31 -0400

    create nodes and move dictionary

---
+++ Project 1/solution_q1.py
@@ -0,0 +1,23 @@
+#create node system that saves each state to track the path from the initial to the goal state
+class node:
+    def __init__(self, state):
+        self.state = state
+        self.parent = None
+        self.move = None
+        self.blankLocation = self.state.index("_") #get matrix coordinates of the blank location
+
+class puzzleSolver:
+    #create a dictionary of possible moves for each coordinate. This helps prevent the program from giving indexErrors when checking for possible moves by blocking out of index moves before they happen
+    possibleMoves = {
+        "00": ['D','R'],
+        "01": ['D','R','L'],
+        "02": ['D','L'],
+        "10": ['D','R','U'],
+        "11": ['D','R','L','U'],
+        "12": ['D','L','U'],
+        "20": ['R','U'],
+        "21": ['R','L','U'],
+        "22": ['L','U']
+    }
+    def isSolved(self, current, goal):
+        return current.state == goal
