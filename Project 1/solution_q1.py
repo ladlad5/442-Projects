1c30f72c267c73d0c9eda386a573f1aa293f88c1
ladlad5 <38514927+ladlad5@users.noreply.github.com>
2023-09-24 10:29:40 -0400

    BFS work

--- Project 1/solution_q1.py
+++ Project 1/solution_q1.py
@@ -11,11 +11,15 @@ class Node:
         self.parent = None
         self.move = None
         self.blankLocation = findBlank(state) #get matrix coordinates of the blank location
+        self.level = 0
     def findBlank(self, matrix):
             for x in range(len(matrix)):
                 for h in range(len(matrix[x])):
                     if matrix[x][h] == "_":
                         return str(x)+str(h)
+    def getState(self):
+        tups = tuple([tuple(x) for x in self.state])
+        return tups
 
 '''class puzzleSolver:
     #create a dictionary of possible moves for each coordinate. This helps prevent the program from giving indexErrors when checking for possible moves by blocking out of index moves before they happen
@@ -106,7 +110,7 @@ print(solve.solverDFS(scramble))
 '''
 class puzzleSolver:
     #create a dictionary of possible moves for each coordinate. This helps prevent the program from giving indexErrors when checking for possible moves by blocking out of index moves before they happen
-    goalState = [['_',1,2],[3,4,5],[6,7,8]]
+    goalState = (('_',1,2),(3,4,5),(6,7,8))
     possibleMoves = {
         "00": ['D','R'],
         "01": ['D','R','L'],
@@ -118,6 +122,46 @@ class puzzleSolver:
         "21": ['R','L','U'],
         "22": ['L','U']
     }
+    move = {
+        'D' : (1,0),
+        'U' : (-1,0),
+        'L' : (0,-1),
+        'R' : (0,1)
+    }
+    opposites = {
+        'U': 'D',
+        'D': 'U',
+        'L': 'R',
+        'R': 'L'
+    }
+    def successors(self, parent):
+            newMove = None
+            possibleMoves = self.possibleMoves[parent.blankLocation]
+            children = []
+            for x in possibleMoves:
+                swapCoordinate = self.move[x]
+                print(f"{swapCoordinate=}")
+                '''moveNumber = parent.state[int(parent.blankLocation[0])+1][int(parent.blankLocation[1])]
+                parent.move = str(moveNumber)+ self.opposites[x]'''
+                newMove = Node([x[:] for x in parent.state])
+                newMove.parent = parent
+                parent.blankLocation = parent.findBlank(parent.state)
+                print(f"{parent.blankLocation=}")
+                blankRow = int(parent.blankLocation[0])
+                print(f"{blankRow=}")
+                blankCol = int(parent.blankLocation[1])
+                print(f"{blankCol=}")
+                newMove.state[blankRow][blankCol] = newMove.state[blankRow+swapCoordinate[0]][blankCol+swapCoordinate[1]]
+                newMove.state[blankRow+swapCoordinate[0]][blankCol+swapCoordinate[1]] = '_'
+                '''(newMove.state[int(parent.blankLocation[0])+swapCoordinate[0]][int(parent.blankLocation[1])], 
+                 newMove.state[int(parent.blankLocation[0])][int(parent.blankLocation[1])]) = (
+                      newMove.state[int(parent.blankLocation[0])][int(parent.blankLocation[1])], 
+                      newMove.state[int(parent.blankLocation[0])+swapCoordinate[0]][int(parent.blankLocation[1])+swapCoordinate[1]])'''
+                newMove.blankLocation=newMove.findBlank(newMove.state)
+                newMove.level = parent.level+1
+                children.append(newMove)
+            return children
+
     def solver(self, node):
         visitedStates = []
         stack = []
@@ -127,10 +171,6 @@ class puzzleSolver:
         path =[]
         traceMap = {}
         while True:
-            newMoveD = None
-            newMoveL = None
-            newMoveR = None
-            newMoveU = None
             state = stack.pop(-1)
             print(state.state)
             possibleMoves = self.possibleMoves[state.blankLocation]
@@ -148,47 +188,6 @@ class puzzleSolver:
                 return traceMap, state.parent
             #if self.isSolved(state,self.goalState):
                 #return state
-            if 'D' in possibleMoves:
-                moveNumber = state.state[int(state.blankLocation[0])+1][int(state.blankLocation[1])]
-                state.move = str(moveNumber)+'U'
-                newMoveD = Node([x[:] for x in aux])
-                newMoveD.parent = state
-                newMoveD.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMoveD.state[int(state.blankLocation[0])+1][int(state.blankLocation[1])] = newMoveD.state[int(state.blankLocation[0])+1][int(state.blankLocation[1])], newMoveD.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                newMoveD.blankLocation=newMoveD.findBlank(newMoveD.state)
-                stack.append(newMoveD)
-                #newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMove.state[int(state.blankLocation[0])+1][int(state.blankLocation[1])] = newMove.state[int(state.blankLocation[0])+1][int(state.blankLocation[1])], newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                #newMove.blankLocation=newMove.findBlank(newMove.state)
-            if 'R' in possibleMoves:
-                moveNumber = state.state[int(state.blankLocation[0])][int(state.blankLocation[1])+1]
-                state.move = str(moveNumber)+'L'
-                newMoveR = Node([x[:] for x in aux])
-                newMoveR.parent = state
-                newMoveR.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMoveR.state[int(state.blankLocation[0])][int(state.blankLocation[1])+1] = newMoveR.state[int(state.blankLocation[0])][int(state.blankLocation[1])+1], newMoveR.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                newMoveR.blankLocation=newMoveR.findBlank(newMoveR.state)
-                stack.append(newMoveR)
-                #newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])+1] = newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])+1], newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                #newMove.blankLocation=newMove.findBlank(newMove.state)
-            if 'L' in possibleMoves:
-                moveNumber = state.state[int(state.blankLocation[0])][int(state.blankLocation[1])-1]
-                state.move = str(moveNumber)+'R'
-                newMoveL = Node([x[:] for x in aux])
-                newMoveL.parent = state
-                newMoveL.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMoveL.state[int(state.blankLocation[0])][int(state.blankLocation[1])-1] = newMoveL.state[int(state.blankLocation[0])][int(state.blankLocation[1])-1], newMoveL.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                newMoveL.blankLocation=newMoveL.findBlank(newMoveL.state)
-                stack.append(newMoveL)
-                #newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])-1] = newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])-1], newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                #newMove.blankLocation=newMove.findBlank(newMove.state)
-            if 'U' in possibleMoves:
-                moveNumber = state.state[int(state.blankLocation[0])-1][int(state.blankLocation[1])]
-                state.move = str(moveNumber)+'D'
-                newMoveU = Node([x[:] for x in aux])
-                newMoveU.parent = state
-                newMoveU.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMoveU.state[int(state.blankLocation[0])-1][int(state.blankLocation[1])] = newMoveU.state[int(state.blankLocation[0])-1][int(state.blankLocation[1])], newMoveU.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                newMoveU.blankLocation=newMoveU.findBlank(newMoveU.state)
-                stack.append(newMoveU)
-                #newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMove.state[int(state.blankLocation[0])-1][int(state.blankLocation[1])] = newMove.state[int(state.blankLocation[0])-1][int(state.blankLocation[1])], newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                #newMove.blankLocation=newMove.findBlank(newMove.state)
-            children = [newMoveD,newMoveR,newMoveL,newMoveU]
             for child in children:
                 traceMap[child] = state #this line was added
             counter += 1
@@ -196,90 +195,58 @@ class puzzleSolver:
     
     
     def solverBFS(self, node):
-        visitedStates = []
-        stack = []
-        stack.append(node)
-        visitedStates = []
-        counter = 0
-        path =[]
-        while True:
-            state = stack.pop(0)
-            print(state.state)
-            possibleMoves = self.possibleMoves[state.blankLocation]
-            aux = []
-            if state.state in visitedStates:
-                continue
-            else:
-                visitedStates.append(state.state)
-                print("NEW STATE" + str(state.state))
-            for x in range(len(state.state)):
-                aux.append([])
-                for h in range(len(state.state[x])):
-                    aux[x].append(state.state[x][h])
-            if self.isSolved(state,self.goalState):
-                return path
-            #if self.isSolved(state,self.goalState):
-                #return state
-            if 'D' in possibleMoves:
-                moveNumber = state.state[int(state.blankLocation[0])+1][int(state.blankLocation[1])]
-                state.move = str(moveNumber)+'U'
-                newMoveD = Node([x[:] for x in aux])
-                newMoveD.parent = state
-                newMoveD.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMoveD.state[int(state.blankLocation[0])+1][int(state.blankLocation[1])] = newMoveD.state[int(state.blankLocation[0])+1][int(state.blankLocation[1])], newMoveD.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                newMoveD.blankLocation=newMoveD.findBlank(newMoveD.state)
-                stack.append(newMoveD)
-                #newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMove.state[int(state.blankLocation[0])+1][int(state.blankLocation[1])] = newMove.state[int(state.blankLocation[0])+1][int(state.blankLocation[1])], newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                #newMove.blankLocation=newMove.findBlank(newMove.state)
-            if 'R' in possibleMoves:
-                moveNumber = state.state[int(state.blankLocation[0])][int(state.blankLocation[1])+1]
-                state.move = str(moveNumber)+'L'
-                newMoveR = Node([x[:] for x in aux])
-                newMoveR.parent = state
-                newMoveR.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMoveR.state[int(state.blankLocation[0])][int(state.blankLocation[1])+1] = newMoveR.state[int(state.blankLocation[0])][int(state.blankLocation[1])+1], newMoveR.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                newMoveR.blankLocation=newMoveR.findBlank(newMoveR.state)
-                stack.append(newMoveR)
-                #newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])+1] = newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])+1], newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                #newMove.blankLocation=newMove.findBlank(newMove.state)
-            if 'L' in possibleMoves:
-                moveNumber = state.state[int(state.blankLocation[0])][int(state.blankLocation[1])-1]
-                state.move = str(moveNumber)+'R'
-                newMoveL = Node([x[:] for x in aux])
-                newMoveL.parent = state
-                newMoveL.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMoveL.state[int(state.blankLocation[0])][int(state.blankLocation[1])-1] = newMoveL.state[int(state.blankLocation[0])][int(state.blankLocation[1])-1], newMoveL.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                newMoveL.blankLocation=newMoveL.findBlank(newMoveL.state)
-                stack.append(newMoveL)
-                #newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])-1] = newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])-1], newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                #newMove.blankLocation=newMove.findBlank(newMove.state)
-            if 'U' in possibleMoves:
-                moveNumber = state.state[int(state.blankLocation[0])-1][int(state.blankLocation[1])]
-                state.move = str(moveNumber)+'D'
-                newMoveU = Node([x[:] for x in aux])
-                newMoveU.parent = state
-                newMoveU.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMoveU.state[int(state.blankLocation[0])-1][int(state.blankLocation[1])] = newMoveU.state[int(state.blankLocation[0])-1][int(state.blankLocation[1])], newMoveU.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                newMoveU.blankLocation=newMoveU.findBlank(newMoveU.state)
-                stack.append(newMoveU)
-                #newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])], newMove.state[int(state.blankLocation[0])-1][int(state.blankLocation[1])] = newMove.state[int(state.blankLocation[0])-1][int(state.blankLocation[1])], newMove.state[int(state.blankLocation[0])][int(state.blankLocation[1])]
-                #newMove.blankLocation=newMove.findBlank(newMove.state)
-            counter += 1
-            print(counter)
+        frontier = [node]
+        reached = set()
+        solution = None
+        numExpanded = 0
+        while frontier:
+            parent = frontier.pop(0)
+            numExpanded += 1
+            print(f"{parent.state=}")
+            print(f"{reached=}")
+            for child in self.successors(parent):
+                state = child.getState()
+                print(f"{state=}")
+                if self.isSolved(state):
+                    return child
+                elif state not in reached:
+                    reached.add((state),)
+                    frontier.append(child)
+            print(f"{numExpanded=}")
+        return solution
+
+
             
-    def isSolved(self, current, goal):
-        return current.state == goal
+    def isSolved(self, current):
+        return current == self.goalState
     
 scramble = Node([[7,2,4],[5,'_',6],[8,3,1]])
 solve = puzzleSolver()
-result = solve.solver(scramble)
-moves = []
+result = solve.solverBFS(scramble)
+print(result)
+'''moves = []
+levels = []
 curr = result[1]
 result = result[0]
-'''while curr is not None:
+while curr is not None:
     moves.append(curr.move)
-    curr = curr.parent'''
+    curr = curr.parent
 print(len(moves))
 while (curr != None):
-    moves.append(curr.move)
+    if curr.level not in levels:
+        moves.append(curr.move)
+        levels.append(curr.level)
     if curr.parent is not None:
         curr = curr.parent
     else:
         curr = None
-print(moves)
+opposite = {
+    'U' : 'D',
+    'D': 'U',
+    'L': 'R',
+    'R':'L'
+    }
+new = []
+for x in range(len(moves)):
+    new.append
+print(moves)'''
