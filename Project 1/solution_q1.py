29cb780db8c4734c1daa983415f774a7f5b883bd
ladlad5 <38514927+ladlad5@users.noreply.github.com>
2023-10-05 13:33:07 -0400

    cleanup

--- Project 1/solution_q1.py
+++ Project 1/solution_q1.py
@@ -23,93 +23,7 @@ class Node:
         tups = tuple([tuple(x) for x in self.state])
         return tups
 
-'''class puzzleSolver:
-    #create a dictionary of possible moves for each coordinate. This helps prevent the program from giving indexErrors when checking for possible moves by blocking out of index moves before they happen
-    goalState = [['_',1,2],[3,4,5],[6,7,8]]
-    possibleMoves = {
-        "00": ['D','R'],
-        "01": ['D','R','L'],
-        "02": ['D','L'],
-        "10": ['D','R','U'],
-        "11": ['D','R','L','U'],
-        "12": ['D','L','U'],
-        "20": ['R','U'],
-        "21": ['R','L','U'],
-        "22": ['L','U']
-    }
-    visitedStates = []
-    def solverDFS(self, node):
-        #print(node.state)
-        possibleMoves = self.possibleMoves[node.blankLocation]
-        aux = []
-        for x in range(len(node.state)):
-            aux.append([])
-            for h in range(len(node.state[x])):
-                aux[x].append(node.state[x][h])
-        newMove = Node(aux.copy())
-        newMove.parent = node
-        if self.isSolved(node,self.goalState):
-            print("found")
-            return []
-        if node.state in self.visitedStates:
-            return None
-        else:
-            self.visitedStates.append(node.state)
-            x = open("log.txt", 'a')
-            x.write(str(node.state))
-            x.write("\n")
-            x.close()
-        #print("states" + str(self.visitedStates[-1]))
-        if 'D' in possibleMoves:
-            moveNumber = node.state[int(node.blankLocation[0])+1][int(node.blankLocation[1])]
-            node.move = str(moveNumber)+'U'
-            newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])], newMove.state[int(node.blankLocation[0])+1][int(node.blankLocation[1])] = newMove.state[int(node.blankLocation[0])+1][int(node.blankLocation[1])], newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])]
-            newMove.blankLocation=newMove.findBlank(newMove.state)
-            result = self.solverDFS(newMove)
-            if result is not None:
-                return result.append(node.move)
-            newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])], newMove.state[int(node.blankLocation[0])+1][int(node.blankLocation[1])] = newMove.state[int(node.blankLocation[0])+1][int(node.blankLocation[1])], newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])]
-            newMove.blankLocation=newMove.findBlank(newMove.state)
-        if 'R' in possibleMoves:
-            moveNumber = node.state[int(node.blankLocation[0])][int(node.blankLocation[1])+1]
-            node.move = str(moveNumber)+'L'
-            newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])], newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])+1] = newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])+1], newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])]
-            newMove.blankLocation=newMove.findBlank(newMove.state)
-            result = self.solverDFS(newMove)
-            if result is not None:
-                return result.append(node.move)
-            newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])], newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])+1] = newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])+1], newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])]
-            newMove.blankLocation=newMove.findBlank(newMove.state)
-        if 'L' in possibleMoves:
-            moveNumber = node.state[int(node.blankLocation[0])][int(node.blankLocation[1])-1]
-            node.move = str(moveNumber)+'R'
-            newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])], newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])-1] = newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])-1], newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])]
-            newMove.blankLocation=newMove.findBlank(newMove.state)
-            result = self.solverDFS(newMove)
-            if result is not None:
-                return result.append(node.move)
-            newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])], newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])-1] = newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])-1], newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])]
-            newMove.blankLocation=newMove.findBlank(newMove.state)
-        if 'U' in possibleMoves:
-            moveNumber = node.state[int(node.blankLocation[0])-1][int(node.blankLocation[1])]
-            node.move = str(moveNumber)+'D'
-            newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])], newMove.state[int(node.blankLocation[0])-1][int(node.blankLocation[1])] = newMove.state[int(node.blankLocation[0])-1][int(node.blankLocation[1])], newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])]
-            newMove.blankLocation=newMove.findBlank(newMove.state)
-            result = self.solverDFS(newMove)
-            if result is not None:
-                return result.append(node.move)
-            newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])], newMove.state[int(node.blankLocation[0])-1][int(node.blankLocation[1])] = newMove.state[int(node.blankLocation[0])-1][int(node.blankLocation[1])], newMove.state[int(node.blankLocation[0])][int(node.blankLocation[1])]
-            newMove.blankLocation=newMove.findBlank(newMove.state)
-        
 
-    def isSolved(self, current, goal):
-        return current.state == goal
-    
-#scramble = Node([[7,2,4],[5,'_',6],[8,3,1]])
-scramble = Node([[2,1,'_'],[3,4,5],[6,7,8]])
-solve = puzzleSolver()
-print(solve.solverDFS(scramble))
-'''
 class puzzleSolver:
     #create a dictionary of possible moves for each coordinate. This helps prevent the program from giving indexErrors when checking for possible moves by blocking out of index moves before they happen
     goalState = (('_',1,2),(3,4,5),(6,7,8))
@@ -142,26 +56,16 @@ class puzzleSolver:
             children = []
             for x in possibleMoves:
                 swapCoordinate = self.move[x]
-                #print(f"{swapCoordinate=}")
-                '''moveNumber = parent.state[int(parent.blankLocation[0])+1][int(parent.blankLocation[1])]
-                parent.move = str(moveNumber)+ self.opposites[x]'''
                 newMove = Node([x[:] for x in parent.state])
                 newMove.parent = parent
                 parent.blankLocation = parent.findBlank(parent.state)
-                #print(f"{parent.blankLocation=}")
                 blankRow = int(parent.blankLocation[0])
-                #print(f"{blankRow=}")
                 blankCol = int(parent.blankLocation[1])
-                #print(f"{blankCol=}")
                 getSwapNum = parent.state[blankRow+swapCoordinate[0]][blankCol+swapCoordinate[1]]
                 currentMove = str(getSwapNum)+str(self.opposites[x])
                 newMove.move = currentMove
                 newMove.state[blankRow][blankCol] = newMove.state[blankRow+swapCoordinate[0]][blankCol+swapCoordinate[1]]
                 newMove.state[blankRow+swapCoordinate[0]][blankCol+swapCoordinate[1]] = '_'
-                '''(newMove.state[int(parent.blankLocation[0])+swapCoordinate[0]][int(parent.blankLocation[1])], 
-                 newMove.state[int(parent.blankLocation[0])][int(parent.blankLocation[1])]) = (
-                      newMove.state[int(parent.blankLocation[0])][int(parent.blankLocation[1])], 
-                      newMove.state[int(parent.blankLocation[0])+swapCoordinate[0]][int(parent.blankLocation[1])+swapCoordinate[1]])'''
                 newMove.blankLocation=newMove.findBlank(newMove.state)
                 newMove.level = parent.level+1
                 children.append(newMove)
@@ -183,12 +87,8 @@ class puzzleSolver:
         while frontier:
             parent = frontier.pop(-1)
             numExpanded += 1
-            #print(f"{numExpanded=}")
-            #print(f"{parent.state=}")
-            #print(f"{reached=}")
             for child in self.successors(parent):
                 state = child.getState()
-                #print(f"{state=}")
                 if self.isSolved(state):
                     print(f"{numExpanded=}")
                     return child
@@ -198,29 +98,6 @@ class puzzleSolver:
         print(f"DFS {numExpanded=}")
         return solution
     
-    '''def solverUCS(self, node):
-        frontier = [node]
-        reached = set()
-        reached.add(node.getState())
-        solution = None
-        numExpanded = 0
-        while frontier:
-            parent = frontier.pop(0)
-            numExpanded += 1
-            #print(f"{numExpanded=}")
-            #print(f"{parent.state=}")
-            #print(f"{reached=}")
-            for child in self.successors(parent):
-                state = child.getState()
-                #print(f"{state=}")
-                if self.isSolved(state):
-                    print(f"{numExpanded=}")
-                    return child
-                elif state not in reached:
-                    reached.add((state),)
-                    frontier.append(child)
-        print(f"{numExpanded=}")
-        return solution'''
 
     
     def solverBFS(self, node):
@@ -322,16 +199,7 @@ class puzzleSolver:
     def isSolved(self, current):
         return current == self.goalState
     
-#scramble = Node([[1,2,5],[3,4,'_'],[6,7,8]])
-#scramble = Node([[8,'_',6],[5,4,7],[2,3,1]])
-#scramble = Node([[1,'_',2],[3,4,5],[6,7,8]])
-#scramble = Node([[7,2,4],[5,'_',6],[8,3,1]])
-#scramble = Node([[1,4,2],[3,'_',5],[6,7,8]])
 solve = puzzleSolver()
-#result = solve.solverBFS(scramble)
-'''result = solve.solverUCS(scramble)
-moves = solve.getMoves(result)
-print(moves)'''
 with open('input.txt') as f:
     puzzle = f.readlines()
 puzzle = str(puzzle[0]).split(',')
