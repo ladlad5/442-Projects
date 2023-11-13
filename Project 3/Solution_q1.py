9b9e567184e93a3279164132e60470320adf2fcd
ladlad5 <38514927+ladlad5@users.noreply.github.com>
2023-11-13 10:31:36 -0500

    needs optimizing but base algorithm works

--- Project 3/Solution_q1.py
+++ Project 3/Solution_q1.py
@@ -1,2 +1,35 @@
 import gymnasium as gym
+from collections import defaultdict
 env = gym.make("Blackjack-v1", render_mode="human") # Initializing environments
+observation, info = env.reset()
+learningRate = 0.9
+gamma = 0.8
+action = 0
+reward = 0
+prevObservation = None
+prevAction = None
+Q = defaultdict(int)
+N = defaultdict(int)
+sumReward = 0
+roundNum = 200
+#def qHelper()
+for x in range(roundNum):
+    N[(prevObservation, prevAction)] += 1
+    Q[(prevObservation, prevAction)] += (learningRate*(N[(prevObservation, prevAction)]) * 
+                                         (reward + (gamma * max(Q[(observation, 1)] - Q[(prevObservation, prevAction)], 
+                                                                Q[(observation, 0)] - Q[(prevObservation, prevAction)]))))
+    if (Q[(observation, 0)] * N[(observation, 0)] > Q[(observation, 1)] * N[(observation, 1)]): #call q learning helper function
+        action = 0
+    else:
+        action = 1
+    prevObservation = observation
+    prevAction = action
+    sumReward += reward
+    avgReward = sumReward/(x+1)
+    print(f'{avgReward=}')
+    observation, reward, terminated, truncated, info = env.step(action)
+    if terminated or truncated:
+        observation, info = env.reset()
+avgReward = sumReward/roundNum
+print(f'{avgReward=}')
+env.close()
