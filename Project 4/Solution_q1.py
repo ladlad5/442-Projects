b1292caf55eacf82afe393e51cb9133200659e35
ladlad5 <38514927+ladlad5@users.noreply.github.com>
2023-12-13 23:20:27 -0500

    seems to work

--- Project 4/Solution_q1.py
+++ Project 4/Solution_q1.py
@@ -1,4 +1,4 @@
-
+from collections import defaultdict
 class CPT:
     def __init__(self, name, table, variables, parents=None, children=None):
         self.name = name
@@ -7,6 +7,7 @@ class CPT:
         self.children = children
         self.variables = variables
 ExpectedPoss = {"john": [True, False], "mary": [True, False], "alarm": [True, False], "burglary": [True, False], "earthquake": [True ,False]}   
+ExpectedPoss = defaultdict(lambda: [True, False])
 def assign(assigned, tba, poss):
     assignments = []
     if len(tba) == 0:
@@ -22,29 +23,56 @@ expected = [{'A': True, 'B': 1, 'C': 'c'}, {'A': True, 'B': 2, 'C': 'c'}, {'A': 
 #assert expected == assign({}, list(poss.keys()), poss), assign({}, list(poss.keys()), poss)
 
 
-def makeFactor(var, evidence, factors):
-    factor = var
+def makeFactor(var, evidence, factors, hidden,query):
+    if var.name in evidence:
+        varIndex = var.variables.index(var.name)
+        newDict = {}
+        for entry in var.table:
+            if entry[1] == evidence[var.name]:
+                newDict[(entry[0],)] = var.table[entry]
+        factor = CPT(var.name, newDict, [var.variables[0]], var.parents, var.children)
+    else:
+        factor = var
+        if var.name != query:
+            hidden.append(var.name)
+    normalize(factor)
     factors.append(factor)
-    return factors
+    return factors, hidden
+    #return factors
 
-def sumOut(var, factors):
+def sumOut(var, factors, last=False):
+    containingFactors = []
+    notContainingFactors = []
+    for factor in factors:
+        if var in factor.variables:
+            containingFactors.append(factor)
+        else:
+            notContainingFactors.append(factor)
+    if not last:
+        factor = pointWiseProd(None, containingFactors)
+    varIndex = factor.variables.index(var)
+    newTotal = defaultdict(int)
+    for row in factor.table:
+        newrow = tuple(list(row[0:varIndex]) + list(row[varIndex+1:]))
+        newTotal[newrow] += factor.table[row]
+    #newFactor = CPT(factor.name, dict(newTotal), factor.variables[0:varIndex] + factor.variables[varIndex+1:], factor.parents, factor.children)
+    newFactor = CPT(factor.name, dict(newTotal), [x for x in factor.variables if x != var], factor.parents, factor.children)
+    if not last:
+        normalize(newFactor)
+    notContainingFactors.append(newFactor)
+    return notContainingFactors
     pass
 
-def pointWiseProd(var, factors):
+def pointWiseProd(var, factors, last=False):
     #make an expectedposs subdictionary from the master using variable names from factors
     subExpectedPoss = {}
     factorNames = []
-    '''for x in factors:
-        for factor in factors:
-            factorNames.append(factor.name)
-            for h in factor.variables:
-                if h in ExpectedPoss and h not in subExpectedPoss:
-                    subExpectedPoss[h] = ExpectedPoss[h]'''
+    if last:
+        factors[1].variables.pop(1)
     for factor in factors:
         factorNames.append(factor.name)
         for h in factor.variables:
-            if h in ExpectedPoss and h not in subExpectedPoss:
-                subExpectedPoss[h] = ExpectedPoss[h]
+            subExpectedPoss[h] = ExpectedPoss[h]
     pointWiseAssignment = assign({}, list(subExpectedPoss.keys()), subExpectedPoss)
     varOrder = list(subExpectedPoss.keys())
     newCPT = {}
@@ -57,20 +85,27 @@ def pointWiseProd(var, factors):
         for factor in factors:
             lookupTup = []
             for variable in factor.variables:
-                lookupTup.append(auxDic[variable])
+                if '*' not in variable or last:
+                    lookupTup.append(auxDic[variable])
             lookupTup = tuple(lookupTup)
             #with lookupTup find the desired row in the CPT table
             probProduct *= factor.table[lookupTup]
-        newCPT[assignment + (True,)] = probProduct
-        newCPT[assignment + (False,)] = 1 - probProduct
+        newCPT[assignment] = probProduct
+        #newCPT[assignment + (True,)] = probProduct
+       # newCPT[assignment + (False,)] = 1 - probProduct
     newName = "*"
     newName = newName.join(factorNames)
     return CPT(newName,
                newCPT,varOrder + [newName], factors[0].parents,factors[0].children)# create new CPT
         
             
-
+def normalize(factor):
+    total = sum(list(factor.table.values()))
+    for condition in factor.table:
+        factor.table[condition] /= total
+    return factor
     pass
+
 pointWiseExample = [
     CPT("john", {
         (True,True): 0.9,
@@ -86,22 +121,36 @@ pointWiseExample = [
     },["alarm", "mary"], ["alarm"]),
 ]
 PWExpectedPoss = {"john": [True, False], "mary": [True, False], "alarm": [True, False]}
-PWExpectedAssignments = assign({}, list(PWExpectedPoss.keys()), PWExpectedPoss)
+#PWExpectedAssignments = assign({}, list(PWExpectedPoss.keys()), PWExpectedPoss)
 #print(PWExpectedAssignments)
 
 PWExpected = CPT("john*mary",{
     (True, True, True): 0.9*0.7, (True, True, False): 0.05*0.01, (True, False, True): 0.9*0.3, (True, False, False):0.05*0.99, 
     (False, True, True): 0.1*0.7, (False, True, False): 0.95*0.01, (False, False, True):0.1*0.3, (False, False, False):0.95*0.99
 }, ["john", "mary", "alarm", "john*mary"])
-print(pointWiseProd(None, pointWiseExample).table)
+#test = pointWiseProd(None, pointWiseExample)
+#print(test.table)
+#print(sumOut("alarm", test).table)
+
+'''def varElim(X, e, bn):
+    factors = []
+    for var in bn:
+        factors = makeFactor(bn[var], e, factors)
+        if (not bn[var].name == X and not bn[var].name in e):
+            factors = sumOut(bn[var], factors)
+    return normalize(pointWiseProd(None, factors))'''
 
 def varElim(X, e, bn):
     factors = []
+    hidden = []
     for var in bn:
-        factors = makeFactor(bn[var], e, bn)
-        if (not var.name == query or not var.name in e):
-            factors = sumOut(bn[var], factors)
-    pass
+        factors, hidden = makeFactor(bn[var], e, factors, hidden, X)
+    hiddenVar = True
+    for var in hidden: 
+        factors = sumOut(var, factors)
+    last = pointWiseProd(None, factors, True)
+    last = sumOut("burglary", [last], True)
+    return last
 
 
 
@@ -129,17 +178,19 @@ bayesNet = { #Manually ordered for this test case
         (False, False, True): 0.001,
         (False, False, False): 0.999,
     },["burglary", "earthquake", "alarm"],["burglary", "earthquake"], ["john", "mary"]),
-    "burglary": CPT("burglary", {
-        (True,): 0.001,
-        (False,): 0.999
-    },["burglary"],None,["alarm"]),
     "earthquake": CPT("earthquake",{
         (True,): 0.002,
         (False,): 0.998
-    },["earthquake"],None,["alarm"])
+    },["earthquake"],None,["alarm"]),
+    "burglary": CPT("burglary", {
+        (True,): 0.001,
+        (False,): 0.999
+    },["burglary"],None,["alarm"])
 }
 query = "burglary"
 evidence = {
     "john": True
 }
-#varElim(query, evidence, bayesNet)
+test = varElim(query, evidence, bayesNet)
+#print(varElim(query, evidence, bayesNet).table)
+print(f'Probability of a burglary given john called: {test[0].table}')
